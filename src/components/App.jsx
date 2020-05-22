c66f31f6c9ac0cb4be7e4fe59c354fb3165e4ddc
dd-tech-nz <ddumolo@hotmail.com>
2020-05-23 10:53:39 +1200

    create external file for card state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CARD_DATA from './card.data.js'
 import Card from './Card'
 import AddCard from './AddCard'
 import DrawButton from './DrawButton'
@@ -6,6 +7,7 @@ import Deck from './Deck'
 import Header from './Header'
 import EditCard from './EditCard'
 import Answers from './Answers'
+import ResetButton from './ResetButton'
 import {Route} from 'react-router-dom'
 
 class App extends Component {
@@ -17,24 +19,7 @@ class App extends Component {
     this.editCard = this.editCard.bind(this)
 
     this.state = {
-      cards: [{
-        id: 1,
-        question: 'What is a higher order function?',
-        answer: 'Function that returns a function or recieves a function as an argument.',
-        imageLink: 'https://lh6.googleusercontent.com/proxy/J8RpWcsFH4gxd7CAcCxy2DyqPnQe1keePlDYRGbZmfr9GoIGN00eL4bDSfSv5kDaPMRDr1DSJ0cVRzcXFs6Ulo3G4n38PNw2rKo55hc0jwfMlXx8ZP_A4Z5h',
-        priority: 5,
-      },{
-        id: 2,
-        question: 'What is a pure function?',
-        answer: 'Function that does not access the DOM or global variables which always returns the same result for given parameters',
-        imageLink: 'https://images.ctfassets.net/wetfygr1eqh0/J1PFSPjxWmsk8iaOUi02O/3c2e988206cd3c7ff5186038820df102/Completion.png?w=400&q=50',
-      },{
-        id: 3,
-        question: 'What is immutablity?',
-        answer: 'When an object or an array does not get mutated when being passed to a function',
-        imageLink: 'https://miro.medium.com/max/600/1*2N0l3bLqaBgmOSIay-uc5w.png',
-        priority: 5,
-      }],
+      cards: CARD_DATA,
       currentCard: {},
       seenCards: [],
       endOfDeck: false
@@ -108,7 +93,7 @@ class App extends Component {
 
   sendEodNotification() {
     let eodFlag = false
-    if (this.state.seenCards.length === this.state.cards.length + 1) eodFlag = true
+    if (this.state.seenCards.length === this.state.cards.length) eodFlag = true
     return eodFlag
 
   }
@@ -119,25 +104,27 @@ class App extends Component {
       <>
       <Route exact path='/' component = {Header} />
       <Route exact path="/" render={() => (
-      <div className="App">
+          <div className="App">
+        <div className="reset">
+          <ResetButton />
+        </div>
         <div className="cardRow">
               <Card
                 // eod={this.state.endOfDeck}
                 question={this.state.currentCard.question}
                 answer={this.state.currentCard.answer}
                 image={this.state.currentCard.imageLink}
-            />
-            </div>
-          <div className="buttonRow">
+              />
+               <div className="buttonRow">
               <DrawButton
                 drawCard={this.updateCard}
                 eod={this.sendEodNotification()}
               />
             </div>
-            <div className="answers">
+              </div>
+              <div className="answers">
               <Answers />
             </div>
-            
         </div>
         )}/>
         <Route path="/AddCard" render={({history}) => (
---
+++ src/components/card.data.js
@@ -0,0 +1,22 @@
+const CARD_DATA = [
+  {
+  id: 1,
+  question: 'What is a higher order function?',
+  answer: 'Function that returns a function or recieves a function as an argument.',
+  imageLink: 'https://lh6.googleusercontent.com/proxy/J8RpWcsFH4gxd7CAcCxy2DyqPnQe1keePlDYRGbZmfr9GoIGN00eL4bDSfSv5kDaPMRDr1DSJ0cVRzcXFs6Ulo3G4n38PNw2rKo55hc0jwfMlXx8ZP_A4Z5h',
+  priority: 5,
+},{
+  id: 2,
+  question: 'What is a pure function?',
+  answer: 'Function that does not access the DOM or global variables which always returns the same result for given parameters',
+  imageLink: 'https://images.ctfassets.net/wetfygr1eqh0/J1PFSPjxWmsk8iaOUi02O/3c2e988206cd3c7ff5186038820df102/Completion.png?w=400&q=50',
+},{
+  id: 3,
+  question: 'What is immutablity?',
+  answer: 'When an object or an array does not get mutated when being passed to a function',
+  imageLink: 'https://miro.medium.com/max/600/1*2N0l3bLqaBgmOSIay-uc5w.png',
+  priority: 5,
+  }
+]
+
+export default CARD_DATA
